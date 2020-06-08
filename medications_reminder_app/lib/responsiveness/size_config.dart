387574df4f236ad438c77a74484b85f2c88a4b83
Crazelu <eberelucky64@gmail.com>
2020-06-08 15:36:20 +0100

    feat: built homescreen, updated SizeConfig class for responsiveness, updated app_theme

--- medications_reminder_app/lib/responsiveness/size_config.dart
+++ medications_reminder_app/lib/responsiveness/size_config.dart
@@ -1,3 +1,20 @@
+import 'package:flutter/material.dart';
+
+///Custom class for scaling text sizes across different screens
 class SizeConfig{
+
+  double yMargin(BuildContext context, double height){
+    double screenHeight =  MediaQuery.of(context).size.height/ 100;
+    return height * screenHeight;
+  }
+  double xMargin(BuildContext context, double width){
+    double screenWidth =  MediaQuery.of(context).size.width/ 100;
+    return width * screenWidth;
+  }
+
+  double textSize(BuildContext context, double textSize){
+    double screenWidth =  MediaQuery.of(context).size.width/ 100;
+    return textSize * screenWidth;
+  }
 
 }
--- medications_reminder_app/lib/ui/home_screen.dart
+++ medications_reminder_app/lib/ui/home_screen.dart
@@ -2,27 +2,75 @@ import 'package:flutter/material.dart';
 
 
 //Note that the colours are #fdfcff and #40b26d
+//! Colours have now been included in the app_theme.dart file so you can use Theme.of(context).whatever_color you like
 //I already added the google fonts package, use poppins
 //I'M COUNTING ON YOU!!!
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: HomeScreen()
+      body: CustomHomeScreen(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).buttonColor,
+        focusColor: Colors.greenAccent,
+        splashColor: Colors.greenAccent,
+        tooltip: "Add a reminder",
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).primaryColorLight
+        ),
+      ),
     );
   }
 }
 
-class Home extends StatefulWidget {
+class CustomHomeScreen extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _CustomHomeScreenState createState() => _CustomHomeScreenState();
 }
 
-class _HomeState extends State<Home> {
+class _CustomHomeScreenState extends State<CustomHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('Home Screen')
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Theme.of(context).primaryColor,
+          expandedHeight: MediaQuery.of(context).size.height * .2,
+          centerTitle: true,
+          pinned: true,
+          title: Text(
+            'Medications Reminder',
+            style: Theme.of(context).textTheme.headline6.copyWith(color:Theme.of(context).primaryColorLight,)
+          ),
+        ),
+
+        //Drug reminders are read from DB and rendered with SliverGrid
+        // SliverGrid.count(
+        //   crossAxisCount: 2,
+        //   children: numbers.map((e) => Row(
+        //     children: <Widget>[
+        //       Container(
+        //         width:50,
+        //         height:50,
+        //         color: Colors.green,
+        //         child: Center(
+        //           child:Text(
+        //             '$e',
+        //             style: TextStyle(
+        //               fontSize: SizeConfig().textSize(context, 4.5)
+        //             ),
+                    
+        //           )
+        //         ),
+        //       ),
+        //       SizedBox(width:20),
+        //     ],
+        //   )).toList(),
+        //   )
+      ],
     );
   }
 }
