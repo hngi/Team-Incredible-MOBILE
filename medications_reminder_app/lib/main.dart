a64f5091a27ee3dfb0a2bfe9febaf6e61fd1b937
Abbie <abbie@github.com>
2020-06-09 00:49:06 +0100

    changed initial route back to splash screen

--- medications_reminder_app/lib/main.dart
+++ medications_reminder_app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/drugsdescription',
+      initialRoute: '/',
       routes: {
         '/': (BuildContext context) => SplashScreen(),
         '/home': (BuildContext context) => HomeScreen(),
