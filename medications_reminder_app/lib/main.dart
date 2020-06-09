8adc9972306a3e692b2ba82770e109c2aea79a47
horlabidemi <thabidemi@gmail.com>
2020-06-09 01:57:56 +0100

    ini

--- medications_reminder_app/lib/main.dart
+++ medications_reminder_app/lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import './ui/home_screen.dart';
 import './ui/splash_screen.dart';
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => SplashScreen(),
--- medications_reminder_app/lib/ui/splash_screen.dart
+++ medications_reminder_app/lib/ui/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 
 
 //Note that the colors are #40b26d(main colour) and sub colour #fdfcff
@@ -14,9 +16,56 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: Center(
-          child: Text('Splash Screen')
-      ),
+        backgroundColor: Colors.green[900],
+        body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+        Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        Container(
+        alignment: Alignment.center,
+        child:
+        Center(
+        child: Image.asset('assets/YouDose.png', width: MediaQuery.of(context).size.width*.9,),
+    )
+
+    ),
+
+    Container(
+    width: 200.0,
+    height: 60.0,
+    margin: EdgeInsets.fromLTRB(0, 180.0, 0, 0.0),
+    decoration: BoxDecoration(
+    color: Colors.green[600],
+    shape: BoxShape.rectangle,
+    borderRadius: BorderRadius.all(
+    Radius.circular(10.0),
+    ),
+    ),
+
+
+
+    child: FlatButton(
+    child: Text('Get Started' ,
+    style: TextStyle(
+    fontSize: 18.0,
+    color: Colors.white,
+    fontWeight: FontWeight.normal,
+    ),
+    ),
+    onPressed: (){
+    Navigator.pushNamed(context, '/home');
+    },
+    ),
+    ),
+    ],
+    ),
+
+        ],
+        ),
     );
   }
+
 }
