aeaeb14602a4cea4be9af859e53349e735a18a64
dev-juri <ojurifemi132@gmail.com>
2020-06-09 14:52:43 +0100

    Linked the splash screen with the home screen

--- medications_reminder_app/lib/ui/splash_screen.dart
+++ medications_reminder_app/lib/ui/splash_screen.dart
@@ -1,10 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:medications_reminder_app/ui/home_screen.dart';
 
 
 //Note that the colors are #40b26d(main colour) and sub colour #fdfcff
 //! Colours have now been included in the app_theme.dart file so you can use Theme.of(context).whatever_color you like
 //I already added the google fonts package, use poppins
 //I'M COUNTING ON YOU!!!
+void main() => runApp(MaterialApp(
+  theme:
+  ThemeData(primaryColor: Colors.green, accentColor: Colors.yellowAccent),
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen(),
+));
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -12,11 +21,69 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),() => Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => HomeScreen()
+    )));
+  }
   Widget build(BuildContext context) {
-    return Scaffold(
-          body: Center(
-          child: Text('Splash Screen')
+    return new Scaffold(
+      body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: Colors.green[900]),
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 50.0,
+                          child: Icon(
+                            Icons.local_pharmacy,
+                            color: Colors.green[900],
+                            size: 50.0,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10.0),
+                        ),
+                        Text(
+                          "MediRemind",
+                          style: TextStyle(
+                              color:Colors.white,
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          CircularProgressIndicator(),
+                          Padding(
+                            padding: EdgeInsets.only(top: 20.0),
+                          ),
+                          Text("Reminder You \n Can Trust", style: TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold),
+                          )
+                        ]
+                    ),
+                  )
+                ]
+            )
+          ]
       ),
     );
+
   }
 }
